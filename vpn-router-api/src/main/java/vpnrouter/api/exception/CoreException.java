0469188b67d7cf2bb02d8d12c78b116fa5af5e38
Kirill Smirnov <mr-skyu@yandex.ru>
2024-08-17 13:33:45 +0300

    client

---
+++ vpn-router-api/src/main/java/vpnrouter/api/exception/CoreException.java
@@ -0,0 +1,8 @@
+package vpnrouter.api.exception;
+
+public class CoreException extends RuntimeException {
+
+    public CoreException(String message) {
+        super(message);
+    }
+}
--- vpn-router-core/src/main/java/vpnrouter/core/service/client/client/ClientServiceImpl.java
+++ vpn-router-core/src/main/java/vpnrouter/core/service/client/client/ClientServiceImpl.java
@@ -1,46 +1,83 @@
 package vpnrouter.core.service.client.client;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import vpnrouter.api.client.ClientCreation;
 import vpnrouter.api.client.ClientService;
 import vpnrouter.api.client.ClientUpdate;
 import vpnrouter.api.client.ClientView;
+import vpnrouter.api.exception.CoreException;
 import vpnrouter.core.service.EntityConverter;
 import vpnrouter.core.service.client.Client;
 import vpnrouter.core.service.client.ClientRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
-import static java.util.Collections.emptyList;
+import static java.util.stream.Collectors.toSet;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ClientServiceImpl implements ClientService {
 
     private final ClientRepository clientRepository;
     private final EntityConverter<Client, ClientView> clientConverter;
+    private final Tunnelling tunnelling;
 
     @Override
     public List<ClientView> getAll() {
-        log.info("Getting all clients");
-        return emptyList();
+        return clientRepository.findAll().stream()
+                .map(clientConverter::toView)
+                .toList();
     }
 
     @Override
     public void add(ClientCreation creation) {
-        log.info("Adding client: {}", creation);
+        if (clientRepository.find(creation.getIpAddress()).isPresent()) {
+            throw new CoreException("Client already exists");
+        }
+        var client = Client.builder()
+                .ipAddress(creation.getIpAddress())
+                .name(creation.getName())
+                .tunnelled(creation.isTunnelled())
+                .lastSwitchedAt(LocalDateTime.now())
+                .build();
+        switchTunnelFor(client);
+        clientRepository.save(client);
     }
 
     @Override
     public void update(String ipAddress, ClientUpdate update) {
-        log.info("Updating client {}: {}", ipAddress, update);
+        var client = clientRepository.find(ipAddress)
+                .orElseThrow(() -> new CoreException("Client not found"));
+        client.setName(update.getName());
+        if (client.isTunnelled() != update.isTunnelled()) {
+            client.setTunnelled(update.isTunnelled());
+            client.setLastSwitchedAt(LocalDateTime.now());
+            switchTunnelFor(client);
+        }
+        clientRepository.save(client);
     }
 
     @Override
     public void remove(String ipAddress) {
-        log.info("Removing client {}", ipAddress);
+        var client = clientRepository.find(ipAddress)
+                .orElseThrow(() -> new CoreException("Client not found"));
+        client.setTunnelled(false);
+        switchTunnelFor(client);
+        clientRepository.deleteIfExists(ipAddress);
+    }
+
+    private void switchTunnelFor(Client client) {
+        var tunnelledIpAddresses = clientRepository.findAll().stream()
+                .filter(Client::isTunnelled)
+                .map(Client::getIpAddress)
+                .collect(toSet());
+        if (client.isTunnelled()) {
+            tunnelledIpAddresses.add(client.getIpAddress());
+        } else {
+            tunnelledIpAddresses.remove(client.getIpAddress());
+        }
+        tunnelling.switchOnOnlyFor(tunnelledIpAddresses);
     }
 }
