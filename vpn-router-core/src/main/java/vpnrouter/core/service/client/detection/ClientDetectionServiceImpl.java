715218c07606a697223572d5c9d36e7ed4c8daf3
Kirill Smirnov <mr-skyu@yandex.ru>
2024-08-17 14:54:29 +0300

    detection

--- vpn-router-core/src/main/java/vpnrouter/core/service/client/detection/ClientDetectionServiceImpl.java
+++ vpn-router-core/src/main/java/vpnrouter/core/service/client/detection/ClientDetectionServiceImpl.java
@@ -1,15 +1,40 @@
 package vpnrouter.core.service.client.detection;
 
-import lombok.extern.slf4j.Slf4j;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import vpnrouter.api.client.ClientDetectionService;
+import vpnrouter.core.service.client.Client;
+import vpnrouter.core.service.client.ClientRepository;
 
-@Slf4j
+import java.time.LocalDateTime;
+
+import static java.util.stream.Collectors.toSet;
+
 @Service
+@RequiredArgsConstructor
 public class ClientDetectionServiceImpl implements ClientDetectionService {
+
+    private final ClientRepository clientRepository;
+    private final ClientDetector clientDetector;
 
     @Override
     public void detectAndSave() {
-        log.info("Detecting and saving clients");
+        var detectedIpAddresses = clientDetector.detectIpAddresses();
+        var existingIpAddresses = clientRepository.findAll().stream()
+                .map(Client::getIpAddress)
+                .collect(toSet());
+        for (var detectedIpAddress : detectedIpAddresses) {
+            if (!existingIpAddresses.contains(detectedIpAddress)) {
+                addClient(detectedIpAddress);
+            }
+        }
+    }
+
+    private void addClient(String ipAddress) {
+        var client = Client.builder()
+                .ipAddress(ipAddress)
+                .lastSwitchedAt(LocalDateTime.now())
+                .build();
+        clientRepository.save(client);
     }
 }
