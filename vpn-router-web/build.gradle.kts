0b9146faf38c71d416de303ccc91a6d9cb92a95d
Aleksandra Grigorieva <grigorieva_a_a@ntik.ru>
2024-08-22 12:00:54 +0300

    Add toggle button dependency

--- vpn-router-web/build.gradle.kts
+++ vpn-router-web/build.gradle.kts
@@ -2,7 +2,13 @@ plugins {
     id("com.vaadin") version "24.4.8"
 }
 
+repositories {
+    mavenCentral()
+    maven(url = "https://vaadin.com/nexus/content/repositories/vaadin-addons/")
+}
+
 dependencies {
     implementation(project(":vpn-router-api"))
     implementation("com.vaadin:vaadin-spring-boot-starter:24.4.8")
+    implementation("com.vaadin.componentfactory:togglebutton:3.0.0")
 }
