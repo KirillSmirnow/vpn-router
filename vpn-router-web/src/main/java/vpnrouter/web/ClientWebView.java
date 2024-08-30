68fa72b1978865d45ef811c0cb5b4ec9dde91d1c
Aleksandra Grigorieva <grigorieva_a_a@ntik.ru>
2024-08-30 16:55:17 +0200

    Remove unused page. Improve equals and hash code methods

--- vpn-router-web/src/main/java/vpnrouter/web/ClientWebView.java
+++ vpn-router-web/src/main/java/vpnrouter/web/ClientWebView.java
@@ -2,37 +2,13 @@ package vpnrouter.web;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
 @Builder
+@EqualsAndHashCode(of = "ipAddress")
 public class ClientWebView {
     private String ipAddress;
     private String name;
     private boolean tunnelled;
-
-    @Override
-    public int hashCode() {
-        if (getIpAddress() != null) {
-            return getIpAddress().hashCode();
-        }
-        return super.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        ClientWebView other = (ClientWebView) obj;
-        if (getIpAddress() == null || other.getIpAddress() == null) {
-            return false;
-        }
-        return getIpAddress().equals(other.getIpAddress());
-    }
 }
