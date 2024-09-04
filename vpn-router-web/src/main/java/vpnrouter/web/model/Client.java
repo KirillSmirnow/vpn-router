e069637d0833b8d39d95dbcac9ee1192c43ae7c7
KirillSmirnov <smirnowku@gmail.com>
2024-09-04 18:56:08 +0300

    no manual mappers

--- vpn-router-web/src/main/java/vpnrouter/web/model/Client.java
+++ vpn-router-web/src/main/java/vpnrouter/web/model/Client.java
@@ -1,6 +1,7 @@
 package vpnrouter.web.model;
 
-import lombok.Builder;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.With;
@@ -11,8 +12,10 @@ import java.util.List;
 
 @Data
 @EqualsAndHashCode(of = "ipAddress")
-@Builder
 public class Client {
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper()
+            .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
     private final String ipAddress;
     @With
@@ -20,24 +23,17 @@ public class Client {
     @With
     private final boolean tunnelled;
 
-    public static Client from(ClientView client) {
-        return Client.builder()
-                .ipAddress(client.getIpAddress())
-                .name(client.getName())
-                .tunnelled(client.isTunnelled())
-                .build();
-    }
-
     public static List<Client> from(List<ClientView> clients) {
         return clients.stream()
                 .map(Client::from)
                 .toList();
     }
 
+    public static Client from(ClientView client) {
+        return OBJECT_MAPPER.convertValue(client, Client.class);
+    }
+
     public ClientUpdate toClientUpdate() {
-        return ClientUpdate.builder()
-                .name(name)
-                .tunnelled(tunnelled)
-                .build();
+        return OBJECT_MAPPER.convertValue(this, ClientUpdate.class);
     }
 }
