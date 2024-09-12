13ec96df8397cf816ef16d6cbf7f8c8bfedee6e6
Kirill Smirnov <mr-skyu@yandex.ru>
2024-09-12 08:11:53 +0300

    update handler

---
+++ vpn-router-web/src/main/java/vpnrouter/web/WebApplication.java
@@ -0,0 +1,10 @@
+package vpnrouter.web;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
+import org.springframework.stereotype.Component;
+
+@Component
+@Push
+public class WebApplication implements AppShellConfigurator {
+}
