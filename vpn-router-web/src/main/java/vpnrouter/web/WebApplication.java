bf107729641e538ffa28fcc28ffbd38f088c4f01
Kirill Smirnov <mr-skyu@yandex.ru>
2024-09-13 15:28:29 +0300

    #30: favicon (#56)

--- vpn-router-web/src/main/java/vpnrouter/web/WebApplication.java
+++ vpn-router-web/src/main/java/vpnrouter/web/WebApplication.java
@@ -2,9 +2,16 @@ package vpnrouter.web;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.server.AppShellSettings;
 import org.springframework.stereotype.Component;
 
 @Component
 @Push
 public class WebApplication implements AppShellConfigurator {
+
+    @Override
+    public void configurePage(AppShellSettings settings) {
+        settings.setPageTitle("VPN Router");
+        settings.addFavIcon("icon", "router.png", "");
+    }
 }
--- vpn-router-web/src/main/java/vpnrouter/web/ui/clients/ClientsPage.java
+++ vpn-router-web/src/main/java/vpnrouter/web/ui/clients/ClientsPage.java
@@ -5,7 +5,6 @@ import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.spring.annotation.UIScope;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +12,6 @@ import vpnrouter.web.ui.AddClientPage;
 
 @UIScope
 @Route("")
-@PageTitle("Clients")
 @CssImport("./styles/styles.css")
 @RequiredArgsConstructor
 public class ClientsPage extends AppLayout {
